cb713396a59e9c235626b0e2417d6d3a0393c998
MorenoB <moreno_bralts@hotmail.com>
2016-09-09 12:37:25 +0200

    Reformatted code

--- src/portforwarder/PortForwarder.java
+++ src/portforwarder/PortForwarder.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package portforwarder;
 
-/**
- *
- * @author Moreno
- */
-public class PortForwarder {
+public class PortForwarder implements Runnable {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
